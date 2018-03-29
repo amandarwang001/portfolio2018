2d7db55064b1da7f8c736bd8e9b38b3eb94651c3
Amanda Wang <amandawang@dijatek.com>
2018-03-28 22:42:31 -0400

    added github icon and link

--- public_html/includes/resume.php
+++ public_html/includes/resume.php
@@ -1,7 +1,11 @@
 <div class="jumbotron jumbotron-fluid mb-5">
-    <div class="container-fluid">
+    <div class="container-fluid text-center">
         <h1 class="display-3 text-center">R&eacute;sum&eacute;</h1>
-        <p class="lead text-center">My passion for front-end design is built at the intersection of technology and the user experience.</p>
-
+        <p class="lead text-center">My passion for front-end design is built at the intersection of technology and the
+            user experience.</p>
+        <a href="https://www.linkedin.com/in/amandarwang/" class="btn-social btn-outline" target="_blank"><img
+                    src="<?= IMAGE_URL ?>linked-in-icon.png" class="img-responsive socialicon" alt="Linked In"></a>
+        <a href="https://github.com/amandarwang001" class="btn-social btn-outline" target="_blank"><img
+                    src="<?= IMAGE_URL ?>GitHub-Mark-120px-plus.png" class="img-responsive socialicon" alt="Github"></a>
     </div>
 </div>
