dfdae76b733d0d8b6d1d608b508a0df9bacc0c9c
Amanda Wang <amandawang@dijatek.com>
2018-03-28 22:19:20 -0400

    home page image update

--- public_html/includes/home.php
+++ public_html/includes/home.php
@@ -9,26 +9,42 @@
     </div>
 </div>
 
-
 <div class="container home">
     <h2 class="text-center">A sampling of websites I have designed.</h2>
     <div class="row text-center mt-5">
 
-        <div class="col-md-6 pr-0">
+        <div class="col-md-6">
             <div class="card blueBackground homeCard">
-                <img class="img-fluid" src="<?= IMAGE_URL ?>chachowtablet.png"
-                     alt="PHP Template">
+                <a href="https://geneomy.com/" target="_blank"><img class="img-fluid" src="<?= IMAGE_URL ?>geneomytablet.jpg"
+                                                                    alt="PHP Template"></a>
+                <div class="card-body p-2">
+                    <h5 class="card-title">Geneomy</h5>
+                    <p class="card-text">Geneomy is a family social network site. It allows family members to connect
+                        to each other and share their life events, as well as pass down their memories onto accounts for
+                        their children.
+                        <br><br>
+                        Front end built with: PHP, HTML, CSS, and MaterializeCSS. Fully mobile-responsive.
+                        <br><br>
+                        Give it a try or view my sample account: amandarwang001@gmail.com <br>PW: abcd1234
+
+                    </p>
+                    <a href="https://geneomy.com/" target="_blank" class="btn btn-red">Visit Geneomy</a>
+                </div>
             </div>
         </div>
-        <div class="col-md-6 pl-0">
+        <div class="col-md-6">
             <div class="card blueBackground homeCard">
+                <a href="http://guoxingmartialarts.com/" target="_blank"><img class="img-fluid" src="<?= IMAGE_URL ?>gxmatablet.jpg"
+                     alt="PHP Template"></a>
                 <div class="card-body p-2">
-                    <h5 class="card-title">ChaChow</h5>
-                    <p class="card-text">ChaChow is a food truck community website, connecting food truck owners with their customers.
+                    <h5 class="card-title">Guo Xing Martial Arts School</h5>
+                    <p class="card-text">Guo Xing Martial Arts School is an internal and external martial arts school
+                        located in Kuraby, QLD, Australia and Orlando, FL, USA.
                         <br><br>
-                        Front end built with: PHP, HTML, CSS, and Bootstrap 4. Fully mobile-responsive.
+                        Front end built with: PHP, HTML, CSS, and MaterializeCSS. Fully mobile-responsive.
                     </p>
-                    <a href="http://chachow.com/" target="_blank" class="btn btn-red">Visit ChaChow</a>
+                    <a href="http://guoxingmartialarts.com/" target="_blank" class="btn btn-red">Visit Guo Xing Martial
+                        Arts School</a>
                 </div>
             </div>
         </div>
@@ -38,103 +54,10 @@
 
 <div class="container home">
     <div class="row text-center mt-5 pt-5">
-        <div class="col-md-6 pr-0">
-            <div class="card blueBackground homeCard">
-                <img class="img-fluid" src="<?= IMAGE_URL ?>gxmatablet.png"
-                     alt="PHP Template">
-
+        <div class="col">
+            <div id="video">
+                <video id="bgvid" src="<?= IMAGE_URL ?>Dijatek_logo_v1.mp4" autoplay muted type="video/mp4">
             </div>
-        </div>
-        <div class="col-md-6 pl-0">
-            <div class="card blueBackground homeCard">
-                <div class="card-body p-2">
-                    <h5 class="card-title">Guo Xing Martial Arts School</h5>
-                    <p class="card-text">Guo Xing Martial Arts School is an internal and external martial arts school
-                        located in Kuraby, QLD, Australia and Orlando, FL, USA.
-                        <br><br>
-                        Front end built with: PHP, HTML, CSS, and MaterializeCSS. Fully mobile-responsive.
-
-                    </p>
-                    <a href="http://guoxingmartialarts.com/" target="_blank" class="btn btn-red">Visit Guo Xing Martial Arts School</a>
-                </div>
-            </div>
-        </div>
-
-    </div>
-</div>
-<!--        <div class="col-md-4">-->
-<!--            <a href="coding">-->
-<!--                <div>-->
-<!--                    <img class="img-fluid" src="-->
-<? //= IMAGE_URL ?><!--placeholder.png" alt="PHP Template">-->
-<!--                </div>-->
-<!--                <div>-->
-<!--                    <h2>CODING</h2>-->
-<!--                    <h3>More information goes here.</h3>-->
-<!--                </div>-->
-<!--            </a>-->
-<!--            <p><a href="coding" class="btn btn-success gray" role="button">LEARN MORE</a></p>-->
-<!--        </div>-->
-<!--        <div class="col-md-4">-->
-<!--            <a href="blog">-->
-<!--                <div>-->
-<!--                    <img class="img-fluid" src="-->
-<? //= IMAGE_URL ?><!--placeholder.png" alt="PHP Template">-->
-<!--                </div>-->
-<!--                <div>-->
-<!--                    <h2>BLOG</h2>-->
-<!--                    <h3>More information goes here.</h3>-->
-<!--                </div>-->
-<!--            </a>-->
-<!--            <p><a href="services" class="btn btn-success blue" role="button">LEARN MORE</a></p>-->
-<!--        </div>-->
-<!--        <div class="col-md-4 text-center">-->
-<!--            <a href="about-us">-->
-<!--                <div>-->
-<!--                    <img class="img-fluid" src="-->
-<? //= IMAGE_URL ?><!--placeholder.png" alt="PHP Template">-->
-<!--                </div>-->
-<!--                <div>-->
-<!--                    <h2>ABOUT US</h2>-->
-<!--                    <h3>More information goes here.</h3>-->
-<!--                </div>-->
-<!--            </a>-->
-<!--            <p><a href="about-us" class="btn btn-success brown" role="button">LEARN MORE</a></p>-->
-
-<div class="row my-5 py-5 text-center">
-    <div class="col-12">
-        <h2 class="emphasisFont2">"A user interface is like a joke. If you have to explain it, it's not that good."
-            - Martin Leblanc
-        </h2>
-        <!--    <div class="col-md-2">-->
-        <!--        <a href="about-us" class="text-center"><img class="img-fluid pt-5" src="-->
-        <? //= IMAGE_URL ?><!--icon.png"-->
-        <!--                                                    alt="PHP Template"></a>-->
-        <!--    </div>-->
-        <!--    <div class="col-md-10 py-5" id="homeabout">-->
-        <!--        <h2 id="quote">“A little about company”</h2>-->
-        <!--        <p>-->
-        <!--            Vestibulum aliquam sollicitudin justo ut pellentesque. Etiam vel dictum neque. Nulla convallis mi sem,-->
-        <!--            ac egestas dui interdum vitae. Quisque accumsan ex eget sem dapibus euismod. Proin ac massa a nisl-->
-        <!--            sagittis tristique. Aliquam luctus id elit quis luctus.-->
-        <!--        </p>-->
-        <!--        <p><a href="contact-us" class="btn btn-success hidden-xs-up" role="button">CONTACT US-->
-        <!--                TODAY</a>-->
-        <!--        </p>-->
-        <!--    </div>-->
-    </div>
-</div>
-
-<div class="container home">
-    <div class="row text-center mt-5">
-        <div class="col-md-6 pr-0">
-            <div class="card blueBackground homeCard">
-                <img class="img-fluid" src="<?= IMAGE_URL ?>dijatek_old.png"
-                     alt="PHP Template">
-            </div>
-        </div>
-<!--        TODO: swap screenshot with opening video-->
-        <div class="col-md-6 pl-0">
             <div class="card blueBackground homeCard">
                 <div class="card-body p-2">
                     <h5 class="card-title">Dijatek</h5>
@@ -152,62 +75,42 @@
     </div>
 </div>
 
-<div class="row my-5 py-5 text-center">
-    <div class="col-12">
-        <h2 class="emphasisFont3">"What separates design from art is that design is meant to be...functional."
-            - Cameron Moll
-        </h2>
-        <!--    <div class="col-md-2">-->
-        <!--        <a href="about-us" class="text-center"><img class="img-fluid pt-5" src="-->
-        <? //= IMAGE_URL ?><!--icon.png"-->
-        <!--                                                    alt="PHP Template"></a>-->
-        <!--    </div>-->
-        <!--    <div class="col-md-10 py-5" id="homeabout">-->
-        <!--        <h2 id="quote">“A little about company”</h2>-->
-        <!--        <p>-->
-        <!--            Vestibulum aliquam sollicitudin justo ut pellentesque. Etiam vel dictum neque. Nulla convallis mi sem,-->
-        <!--            ac egestas dui interdum vitae. Quisque accumsan ex eget sem dapibus euismod. Proin ac massa a nisl-->
-        <!--            sagittis tristique. Aliquam luctus id elit quis luctus.-->
-        <!--        </p>-->
-        <!--        <p><a href="contact-us" class="btn btn-success hidden-xs-up" role="button">CONTACT US-->
-        <!--                TODAY</a>-->
-        <!--        </p>-->
-        <!--    </div>-->
-    </div>
-</div>
-
 <div class="container home mb-5 pb-5">
     <div class="row text-center mt-5 mb-5 pb-5">
-        <div class="col-md-6 pr-0">
+        <div class="col-md-6">
             <div class="card blueBackground homeCard">
-                <img class="img-fluid" src="<?= IMAGE_URL ?>geneomytablet.png"
-                     alt="PHP Template">
-
-            </div>
-        </div>
-        <div class="col-md-6 pl-0">
-            <div class="card blueBackground homeCard">
+                <a href="http://pathogend.com/" target="_blank"><img class="img-fluid" src="<?= IMAGE_URL ?>pathogend.jpg"
+                                                                   alt="PHP Template"></a>
                 <div class="card-body p-2">
-                    <h5 class="card-title">Geneomy</h5>
-                    <p class="card-text">Geneomy is a family social network site. It allows family members to connect
-                        to each other and share their life events, as well as pass down their memories onto accounts for
-                        their children.
+                    <h5 class="card-title">Pathogend</h5>
+                    <p class="card-text">blah blah blah
                         <br><br>
-                        Front end built with: PHP, HTML, CSS, and MaterializeCSS. Fully mobile-responsive.
-                        <br><br>
-                        Give it a try or view my sample account: amandarwang001@gmail.com <br>PW: abcd1234
-
+                        Front end built with: PHP, HTML, CSS, and Bootstrap 4. Fully mobile-responsive.
                     </p>
-                    <a href="https://geneomy.com/" target="_blank" class="btn btn-red">Visit Geneomy</a>
+                    <a href="http://pathogend.com/" target="_blank" class="btn btn-red">Visit Pathogend</a>
                 </div>
             </div>
         </div>
-
+        <div class="col-md-6">
+            <div class="card blueBackground homeCard">
+                <a href="http://chachow.com/" target="_blank"><img class="img-fluid" src="<?= IMAGE_URL ?>chachowtablet.jpg"
+                                                                   alt="PHP Template"></a>
+                <div class="card-body p-2">
+                    <h5 class="card-title">ChaChow</h5>
+                    <p class="card-text">ChaChow is a food truck community website, connecting food truck owners with
+                        their customers.
+                        <br><br>
+                        Front end built with: PHP, HTML, CSS, and Bootstrap 4. Fully mobile-responsive.
+                    </p>
+                    <a href="http://chachow.com/" target="_blank" class="btn btn-red">Visit ChaChow</a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
 
-<div class="sticky-logo p-2">
+<div class="sticky-logo p-2 hidden-sm-down">
     <div class="sticky-text text-center">
         <a href="tel:8639443986" class="navbar-btn emphasisFont1 text-white"></span>
             <small>(863)-944-3986</strong></small>
