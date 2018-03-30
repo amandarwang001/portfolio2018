3451dffbc1dbe032e2b6d96a9fa6d55e7b23bd59
Amanda Wang <amandawang@dijatek.com>
2018-03-29 22:50:26 -0400

    started resume

--- public_html/includes/resume.php
+++ public_html/includes/resume.php
@@ -9,3 +9,125 @@
                     src="<?= IMAGE_URL ?>GitHub-Mark-120px-plus.png" class="img-responsive socialicon" alt="Github"></a>
     </div>
 </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+            <div class="card blueBackground">
+                <div class="card-body">
+                    <h5 class="card-title">Amanda R Wang</h5>
+                    <p class="card-text">Orlando, FL 32829 | 863-944-3986 | amandarwang001@gmail.com
+                        <a href="tel:8639443986" class="navbar-btn emphasisFont1 text-white"></span>
+                            <small>(863)-944-3986</strong></small>
+                        </a>
+                        <br>
+                        <a href="mailto: amandarwang001@gmail.com" class="footerlink">
+                            <small>amandarwang001@gmail.com</small>
+                        </a>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-9">
+
+
+            
+
+
+
+            Work Experience
+            Dijatek, LLC - Oviedo, FL
+            Director of Operations & Full Stack Developer, Mar. 2017 - current
+            "	Client coordinator responsible for ensuring client needs are understood and met in accordance with agreed proposal, including:
+            o	Main interface between clients and programming staff
+            o	Project management
+            o	Proposal generation and presentation
+            o	Employee supervision
+            o	Deadline management
+            o	Experienced with government contracts
+            "	Lead front-end designer
+            o	Designed UI/UX based on client needs
+            o	Evaluated and refined UI/UX of client websites
+            o	Added accessibility compliance to websites
+            o	Adjusted websites for Search Engine Optimization
+            "	Managed intern program
+            o	Designed program
+            o	Supervised interns in daily activities
+            o	Transitioned interns to full-time employees
+
+            Guo Xing Martial Arts School - Orlando, FL
+            Instructor, July. 2017 - current
+            "	Instruct students in Bagua Zhang and Northern Shaolin Kung Fu
+            "	Primary editor for the translation of modern traditional Chinese martial arts manuals:
+            o	The Operation and Explanation of Taiwan's Hanliu Martial Arts Theory by Kangyi Liu
+            o	Gao Style Cheng School Bagua Zhang Manual by Yukai Chang, PhD
+
+            Natural Medicine Simplified - Orlando, FL
+            Owner, Doctor of Oriental Medicine, 2016
+            "	Designed custom nutritional programs for weight loss based on Traditional Chinese Medicine.
+            "	Wrote a weekly blog educating patients on Traditional Chinese Medicine and Homeopathy.
+            "	Maintained Bookkeeping, Operations and Customer Service.
+            "	Conducted all consultations via HIPAA-compliant, encrypted online video chat.
+
+
+            Independent Acupuncturist Consultant - Orlando, FL
+            Doctor of Oriental Medicine, 2012-2016
+            "	Consulted with Licensed Acupuncturists on difficult and medically-complex cases.
+            "	Consulted with patients on their complicated health issues.
+            "	Educated patients about Traditional Chinese Medicine/Alternative Medicine.
+
+            Advanced Oriental Medicine - Orlando, FL
+            Doctor of Oriental Medicine, 2010-2012
+            "	Diagnosed and treated a wide range of conditions including pain, fibromyalgia, and infertility.
+            "	Designed and prescribed herbal formulas based on Traditional Chinese Medicine.
+            "	Incorporated adjunct modalities such as: gua sha, cupping and moxibustion into treatments.
+            "	Developed and implemented a lowered-cost Community Acupuncture program.
+
+
+        </div>
+        <div class="col-md-3">
+
+            <div class="card redBackground">
+                <div class="card-body">
+                    <h5 class="card-title">Technology Summary</h5>
+                    <p class="card-text">Technology Summary
+                        <ul>
+                        <li>Browser: HTML, CSS, JavaScript, jQuery, Bootstrap, MaterializeCSS</li>
+                        <li>API Interaction: consuming APIs, JSON, AJAX</li>
+                        <li>Dev Tools: Heroku, Git, GitHub</li>
+                        <li>Databases: MySQL, MongoDB</li>
+                    </ul>
+                    </p>
+                    <a href="https://github.com/amandarwang001" class="btn btn-primary">GitHub</a>
+                    <a href="https://www.linkedin.com/in/amandarwang/" class="btn btn-primary">LinkedIn</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <h4>Education</h4>
+            <p>
+                <u>University of Central Florida Coding Bootcamp -- Orlando, FL</u><br>
+                Certificate in Full-Stack Web Development, September 2016
+                <br><br>
+                <u>Florida College of Integrative Medicine - Orlando, FL</u><br>
+                Master of Science in Acupuncture and Oriental Medicine -- 2010<br>
+                Bachelor of Science in Professional Health Studies -- 2010<br>
+                <br>
+                <u>Eckerd College - St. Petersburg, FL</u><br>
+                Bachelor of Science in Biology - 2006
+
+            </p>
+
+        </div>
+
+    </div>
+</div>
+
