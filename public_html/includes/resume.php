da510f07cfe37e3e80f2ab7800588189d50256d3
Amanda Wang <amandawang@dijatek.com>
2018-04-04 21:43:33 -0400

    styling on resume page

--- public_html/includes/resume.php
+++ public_html/includes/resume.php
@@ -4,9 +4,9 @@
         <p class="lead text-center">My passion for front-end design is built at the intersection of technology and the
             user experience.</p>
         <a href="https://www.linkedin.com/in/amandarwang/" class="btn-social btn-outline" target="_blank"><img
-                    src="<?= IMAGE_URL ?>linked-in-icon.png" class="img-responsive socialicon" alt="Linked In"></a>
+                    src="<?= IMAGE_URL ?>linked-in-icon.png" class="img-responsive linkedinIcon" alt="Linked In"></a>
         <a href="https://github.com/amandarwang001" class="btn-social btn-outline" target="_blank"><img
-                    src="<?= IMAGE_URL ?>GitHub-Mark-120px-plus.png" class="img-responsive socialicon" alt="Github"></a>
+                    src="<?= IMAGE_URL ?>GitHub-Mark-120px-plus.png" class="img-responsive githubIcon" alt="Github"></a>
     </div>
 </div>
 
@@ -32,8 +32,8 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-9 ">
-
+        <div class="col-md-8">
+        <h3 class="pt-3">Work Experience</h3>
 
             <div class="card lightBlueBackground navbarFont p-3 mt-3">
                 <div class="card-body">
@@ -44,6 +44,7 @@
                         and met in accordance with agreed proposals, including:<br><br>
 
                         Main interface between clients and programming staff
+                    </p>
                     <ul>
                         <li>Project management</li>
                         <li>Proposal generation and presentation</li>
@@ -51,14 +52,18 @@
                         <li>Deadline management</li>
                         <li>Experienced with government contracts</li>
                     </ul>
-                    <i>Lead front-end designer</i>
+                    <p>
+                        Lead front-end designer<
+                    </p>
                     <ul>
                         <li>Designed UI/UX based on client needs</li>
                         <li>Evaluated and refined UI/UX of client websites</li>
                         <li>Added accessibility compliance to websites</li>
                         <li>Adjusted websites for Search Engine Optimization</li>
                     </ul>
-                    <i>Managed intern program</i>
+                    <p>
+                        Managed intern program
+                    </p>
                     <ul>
                         <li>Designed program</li>
                         <li>Supervised interns in daily activities</li>
@@ -72,15 +77,20 @@
                 <div class="card-body">
                     <h4 class="card-title">Guo Xing Martial Arts School - Orlando, FL</h4>
                     <h6 class="card-subtitle">Instructor, July. 2017 - current</h6>
-                    <br>
                     <p class="card-text">
-                        Instruct students in Bagua Zhang and Northern Shaolin Kung Fu<br>
-                        Primary editor for the translation of modern traditional Chinese martial arts manuals:
-                    <ul>
-                        <li>The Operation and Explanation of Taiwan's Hanliu Martial Arts Theory by Kangyi Liu</li>
-                        <li>Gao Style Cheng School Bagua Zhang Manual by Yukai Chang, PhD</li>
+                        <ul>
+                        <li>Instruct students in Bagua Zhang and Northern Shaolin Kung Fu</li>
+                        <li>Host and plan annual seminar events</li>
+                        <li>Manage website and SEO</li>
                     </ul>
+<!--                        Primary editor for the translation of modern traditional Chinese martial arts manuals:-->
                     </p>
+<!---->
+<!--                    <ul>-->
+<!--                        <li>The Operation and Explanation of Taiwan's Hanliu Martial Arts Theory by Kangyi Liu</li>-->
+<!--                        <li>Gao Style Cheng School Bagua Zhang Manual by Yukai Chang, PhD</li>-->
+<!--                    </ul>-->
+
                 </div>
             </div>
 
@@ -89,12 +99,15 @@
                 <div class="card-body">
                     <h4 class="card-title">Natural Medicine Simplified - Orlando, FL</h4>
                     <h6 class="card-subtitle">Owner, Doctor of Oriental Medicine, 2016</h6>
-                    <br>
                     <p class="card-text">
-                        " Designed custom nutritional programs for weight loss based on Traditional Chinese Medicine.
-                        " Wrote a weekly blog educating patients on Traditional Chinese Medicine and Homeopathy.
-                        " Maintained Bookkeeping, Operations and Customer Service.
-                        " Conducted all consultations via HIPAA-compliant, encrypted online video chat.
+                    <ul>
+                        <li>Designed custom nutritional programs for weight loss based on Traditional Chinese
+                            Medicine.
+                        </li>
+                        <li>Wrote a weekly blog educating patients on Traditional Chinese Medicine and Homeopathy.</li>
+                        <li>Maintained Bookkeeping, Operations and Customer Service.</li>
+                        <li>Conducted all consultations via HIPAA-compliant, encrypted online video chat.</li>
+                    </ul>
                     </p>
                 </div>
             </div>
@@ -103,36 +116,39 @@
                 <div class="card-body">
                     <h4 class="card-title">Independent Acupuncturist Consultant - Orlando, FL</h4>
                     <h6 class="card-subtitle">Doctor of Oriental Medicine, 2012-2016</h6>
-                    <br>
                     <p class="card-text">
-                        " Consulted with Licensed Acupuncturists on difficult and medically-complex cases.
-                        " Consulted with patients on their complicated health issues.
-                        " Educated patients about Traditional Chinese Medicine/Alternative Medicine.
+                    <ul>
+                        <li>Consulted with Licensed Acupuncturists on difficult and medically-complex cases.</li>
+                        <li>Consulted with patients on their complicated health issues.</li>
+                        <li>Educated patients about Traditional Chinese Medicine/Alternative Medicine.</li>
+                    </ul>
                     </p>
                 </div>
             </div>
 
-            <div class="card lightBlueBackground navbarFont p-3 mt-3">
+            <div class="card lightBlueBackground navbarFont p-3 mt-3 mb-5">
                 <div class="card-body">
                     <h4 class="card-title">Advanced Oriental Medicine - Orlando, FL</h4>
                     <h6 class="card-subtitle">Doctor of Oriental Medicine, 2010-2012</h6>
-                    <br>
                     <p class="card-text">
-                        " Diagnosed and treated a wide range of conditions including pain, fibromyalgia, and
-                        infertility.
-                        " Designed and prescribed herbal formulas based on Traditional Chinese Medicine.
-                        " Incorporated adjunct modalities such as: gua sha, cupping and moxibustion into treatments.
-                        " Developed and implemented a lowered-cost Community Acupuncture program.
+                    <ul>
+                        <li>Diagnosed and treated a wide range of conditions including pain, fibromyalgia, and
+                            infertility.
+                        </li>
+                        <li>Designed and prescribed herbal formulas based on Traditional Chinese Medicine.</li>
+                        <li>Incorporated adjunct modalities such as: gua sha, cupping and moxibustion into treatments.
+                        </li>
+                        <li>Developed and implemented a lowered-cost Community Acupuncture program.</li>
+                    </ul>
                     </p>
                 </div>
             </div>
 
         </div>
-        <div class="col-md-3 mt-3">
+        <div class="col-md-4 mt-3">
+            <h3 class="pb-2">Technology Summary</h3>
             <div class="card redBackground p-3 navbarFont">
                 <div class="card-body">
-                    <h5 class="card-title">Technology Summary</h5>
-                    <p class="card-text">Technology Summary
                     <ul>
                         <li>Browser: HTML, CSS, JavaScript, jQuery, Bootstrap, MaterializeCSS</li>
                         <li>API Interaction: consuming APIs, JSON, AJAX</li>
@@ -140,30 +156,28 @@
                         <li>Databases: MySQL, MongoDB</li>
                     </ul>
                     </p>
-                    <a href="https://github.com/amandarwang001" class="btn btn-primary">GitHub</a>
-                    <a href="https://www.linkedin.com/in/amandarwang/" class="btn btn-primary">LinkedIn</a>
+                </div>
+            </div>
+            <h3 class="pt-3">Education</h3>
+            <div class="card grayBackground navbarFont p-3 mt-3">
+                <div class="card-body">
+                    <h6 class="card-title">University of Central Florida Coding Bootcamp -- Orlando, FL</h6>
+                    <p class="card-subtitle"><small>Certificate in Full-Stack Web Development, September 2016</small></p>
                 </div>
             </div>
 
             <div class="card grayBackground navbarFont p-3 mt-3">
                 <div class="card-body">
-                    <h5 class="card-title"><u>University of Central Florida Coding Bootcamp -- Orlando, FL</u></h5>
-                    <h6 class="card-subtitle">Certificate in Full-Stack Web Development, September 2016</h6>
+                    <h6 class="card-title">Florida College of Integrative Medicine - Orlando, FL</h6>
+                    <p class="card-subtitle"><small>Master of Science in Acupuncture and Oriental Medicine -- 2010<br>
+                            Bachelor of Science in Professional Health Studies -- 2010</small></p>
                 </div>
             </div>
 
-            <div class="card grayBackground navbarFont p-3 mt-3">
+            <div class="card grayBackground navbarFont p-3 mt-3 mb-5">
                 <div class="card-body">
-                    <h5 class="card-title"><u>Florida College of Integrative Medicine - Orlando, FL</u></h5>
-                    <h6 class="card-subtitle">Master of Science in Acupuncture and Oriental Medicine -- 2010<br>
-                        Bachelor of Science in Professional Health Studies -- 2010<br></h6>
-                </div>
-            </div>
-
-            <div class="card grayBackground navbarFont p-3 mt-3">
-                <div class="card-body">
-                    <h5 class="card-title"><u>Eckerd College - St. Petersburg, FL</u></h5>
-                    <h6 class="card-subtitle">Bachelor of Science in Biology - 2006</h6>
+                    <h6 class="card-title">Eckerd College - St. Petersburg, FL</h6>
+                    <p class="card-subtitle"><small>Bachelor of Science in Biology - 2006</small></p>
                 </div>
             </div>
 
